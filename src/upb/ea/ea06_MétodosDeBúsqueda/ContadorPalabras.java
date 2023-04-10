7945bbb442bf844c479ce15d1f75278e3d585abf
JorgeML_UPB <jorge.londono@upb.edu.co>
2023-04-10 11:27:41 -0500

    Implemented wordCount with different symbol tables

--- src/upb/ea/ea06_MétodosDeBúsqueda/ContadorPalabras.java
+++ src/upb/ea/ea06_MétodosDeBúsqueda/ContadorPalabras.java
@@ -1,25 +1,47 @@
 package upb.ea.ea06_MétodosDeBúsqueda;
 
-import java.util.HashMap;
-
-import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.BinarySearchST;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.ST;
+import edu.princeton.cs.algs4.SequentialSearchST;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.Stopwatch;
 
 public class ContadorPalabras {
 
-    public static HashMap<String, Integer> wordCount(int minLen) {
-        // TODO: Completar la implementacion
+    public static SequentialSearchST<String, Integer> wordCount(int minLen, In in) {
 
-        while (!StdIn.isEmpty()) {
-            String word = StdIn.readString();
-
+        SequentialSearchST<String,Integer> st = new SequentialSearchST<>();
+        // BinarySearchST<String,Integer> st = new BinarySearchST<>();
+        // ST<String,Integer> st = new ST<>(); // Basada en arbol binario
+        while (! in.isEmpty() ) {
+            String word = in.readString();
+            if (st.contains(word)) {
+                st.put(word, st.get(word)+1);
+            }
+            else {
+                st.put(word, 1);
+            }
         }
-        return null;
+        return st;
     }
 
     public static void main(String[] args) {
-        HashMap<String, Integer> ts = wordCount(3);
-        // TODO: Mostrar los conteos por cada una de las palabras
+        In fileInput = new In("../gabriel_garcia_marquez_cien_annos_soledad.txt");
+        
+        Stopwatch sw = new Stopwatch();
+        SequentialSearchST<String, Integer> wc = wordCount(3, fileInput);
+        double tiempo = sw.elapsedTime();
+
+        for(String w: wc.keys()) {
+            StdOut.println(w+" : "+wc.get(w));
+        }
+        StdOut.println();
+        StdOut.println("Tiempo: "+tiempo);
+
+        // TODO: Mostrar los conteos por cada una de las palabras en orden descendiente por conteo
+        // TODO: Obtener la M palabras de mayor frecuencia
+
 
     }
 
